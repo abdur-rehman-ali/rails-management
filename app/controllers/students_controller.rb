0f162b32c2aa4740006e243ebf95a90e13f84751
abdur-rehman-ali <alijoyia911@gmail.com>
2022-04-09 07:31:55 +0500

    destroy action implemented

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -36,6 +36,12 @@ class StudentsController < ApplicationController
         end
     end
 
+    def destroy 
+        @student = Student.find(params[:id])
+        @student.destroy
+        redirect_to students_path
+    end
+
     private 
     def students_params
         params.require(:student).permit(:name,:email,:age)
