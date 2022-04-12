2526d508f8f3e28f4357c9de2973441de3c06ddd
abdur-rehman-ali <alijoyia911@gmail.com>
2022-04-13 01:07:51 +0500

    solving preview problem

--- test/mailers/previews/post_mailer_preview.rb
+++ test/mailers/previews/post_mailer_preview.rb
@@ -3,7 +3,7 @@ class PostMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/post_mailer/post_created
   def post_created
-    PostMailer.post_created
+    PostMailer.with(user: Student.first.name).post_created
   end
 
 end
