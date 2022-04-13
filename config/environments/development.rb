ffd438934d902118713dd9bd240240d95afaf98c
abdur-rehman-ali <alijoyia911@gmail.com>
2022-04-13 12:39:21 +0500

    send email using gmail smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  
 
   config.action_mailer.perform_caching = false
 
@@ -60,4 +59,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+
+  #GMAIL SMTP
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost:3000',
+    user_name:            'abdur.rehman@devsinc.com',
+    password:             'snnhutmcvpyjljjp',
+    authentication:       'plain',
+    enable_starttls_auto: true }
 end
